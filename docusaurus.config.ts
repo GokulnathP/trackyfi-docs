97eab43e9a6f794adebd24297f30abed8d5ac7f0
GokulnathP <4gokul2000@gmail.com>
2026-06-26 14:18:27 +0530

    Add Google Search Console verification meta tag

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -23,6 +23,16 @@ const config: Config = {
 
   onBrokenLinks: 'throw',
 
+  headTags: [
+    {
+      tagName: 'meta',
+      attributes: {
+        name: 'google-site-verification',
+        content: 'dOIPFBjwYFaTDLhU9SiFTo1RAcPAG7SHxa0eSuRZDa8',
+      },
+    },
+  ],
+
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
